264eaaaaca891a5408d644df5faea340e6944236
Dencho <denchyaknow@gmail.com>
2021-11-07 21:06:21 -0800

    Made particle responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,8 +3,49 @@
 	//The second argument has to be at least a empty object or a object with your desired options
 	//OverlayScrollbars(document.querySelectorAll("body"), { });
 //});
+//For some yummy particles
 window.onload = function() {
 	Particles.init({
-	  selector: '.background'
+	  selector: '.background',
+	  maxParticles: 450,
+	  responsive: [
+		{
+		  breakpoint: 
+	768
+	,
+		  options: {
+			maxParticles: 
+	200
+	,
+			color: 
+	'#48F2E3'
+	,
+			connectParticles: 
+	false
+		  }
+		}, {
+		  breakpoint: 
+	425
+	,
+		  options: {
+			maxParticles: 
+	100
+	,
+			connectParticles: 
+	true
+		  }
+		}, {
+		  breakpoint: 
+	320
+	,
+		  options: {
+			maxParticles: 
+	0
+	 
+	// disables particles.js
+		  }
+		}
+	  ]
+
 	});
   };
