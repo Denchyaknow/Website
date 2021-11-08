7ca5307b584a95c45b16e216d3e86e902ce6addb
Dencho <denchyaknow@gmail.com>
2021-11-08 08:01:20 -0800

    Add lazy loading

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,7 +4,32 @@
 	//OverlayScrollbars(document.querySelectorAll("body"), { });
 //});
 //For some yummy particles
-
+document.addEventListener("DOMContentLoaded", function() {
+	var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
+	console.log("Video Loader Init!");
+	if ("IntersectionObserver" in window) {
+	  var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+		entries.forEach(function(video) {
+		  if (video.isIntersecting) {
+			for (var source in video.target.children) {
+			  var videoSource = video.target.children[source];
+			  if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+				videoSource.src = videoSource.dataset.src;
+			  }
+			}
+  
+			video.target.load();
+			video.target.classList.remove("lazy");
+			lazyVideoObserver.unobserve(video.target);
+		  }
+		});
+	  });
+  
+	  lazyVideos.forEach(function(lazyVideo) {
+		lazyVideoObserver.observe(lazyVideo);
+	  });
+	}
+  });
 //tsparticles
 // const tsParticles = require("tsparticles");
 // tsParticles.load("tsparticles",
