ee0b2933b52cac37953d55b7e72532e1b3645b11
Dencho <denchyaknow@gmail.com>
2021-11-07 21:10:42 -0800

    Update Particles

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,41 +8,33 @@ window.onload = function() {
 	Particles.init({
 	  selector: '.background',
 	  maxParticles: 450,
-	  responsive: [
+	  color: '#9cc2ff',
+	  connectParticles: true,
+	  responsive: 
+	  [
 		{
-		  breakpoint: 
-	768
-	,
-		  options: {
-			maxParticles: 
-	200
-	,
-			color: 
-	'#48F2E3'
-	,
-			connectParticles: 
-	false
+		  breakpoint: 768,
+		  options: 
+		  {
+			maxParticles: 200,
+			connectParticles: false
 		  }
-		}, {
-		  breakpoint: 
-	425
-	,
-		  options: {
-			maxParticles: 
-	100
-	,
-			connectParticles: 
-	true
+		}, 
+		{
+		  breakpoint: 425,
+		  options: 
+		  {
+			maxParticles: 100,
+			connectParticles: true
 		  }
-		}, {
-		  breakpoint: 
-	320
-	,
-		  options: {
-			maxParticles: 
-	0
+		}, 
+		{
+		  breakpoint: 320,
+		  options: 
+		  {
+			maxParticles: 0
 	 
-	// disables particles.js
+			// disables particles.js
 		  }
 		}
 	  ]
